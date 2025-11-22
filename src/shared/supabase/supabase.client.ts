c621179e875432609be2dadf59d8e8e077aa4df2
AntoineThibi <antoinet@bam.tech>
2025-11-22 15:00:29 +0100

    feat: ✨ add login to the supabase client

--- src/shared/supabase/supabase.client.ts
+++ src/shared/supabase/supabase.client.ts
@@ -1,6 +1,43 @@
 import { createClient } from "@supabase/supabase-js";
+import { AppState, Platform } from "react-native";
+import * as SecureStore from "expo-secure-store";
+
+const SecureStoreAdapter = {
+  getItem: (key: string): Promise<string | null> => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: async (key: string, value: string): Promise<void> => {
+    await SecureStore.setItemAsync(key, value);
+  },
+  removeItem: async (key: string): Promise<void> => {
+    await SecureStore.deleteItemAsync(key);
+  },
+};
 
 export const supabase = createClient(
   "https://ijlpqdcsbajeqcsaunpg.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqbHBxZGNzYmFqZXFjc2F1bnBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyNzkyODcsImV4cCI6MjA3ODg1NTI4N30.Fn5anTTZUnPbCZalst0gKkwHPE8-qZBa9m9pJbrBD78",
+  {
+    auth: {
+      storage: SecureStoreAdapter,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  },
 );
+
+// Tells Supabase Auth to continuously refresh the session automatically
+// if the app is in the foreground. When this is added, you will continue
+// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
+// `SIGNED_OUT` event if the user's session is terminated. This should
+// only be registered once.
+if (Platform.OS !== "web") {
+  AppState.addEventListener("change", async (state) => {
+    if (state === "active") {
+      await supabase.auth.startAutoRefresh();
+    } else {
+      await supabase.auth.stopAutoRefresh();
+    }
+  });
+}
